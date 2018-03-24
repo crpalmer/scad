40cee0809aedf746902e543ded6cd69bbdbe9499
Christopher R. Palmer <crpalmer@gmail.com>
2018-03-24 07:04:18 -0400

    paw-shop: Allow the sh to generate big and small

--- one-offs/paw-shop.sh
+++ one-offs/paw-shop.sh
@@ -1,10 +1,17 @@
 is_big=1
+O=paw-shop
+
+if [ "$1" == --small ]
+then
+    is_big=0
+    O=$O/small
+fi
 
 for letter in P A W S H O
 do
     for which in 0 1
     do
 	echo "Generating $letter-$which"
-        openscad -o paw-shop/$letter-$which.stl -Dletter='"'$letter'"' -Dwhich=$which -Dis_big=$is_big ./paw-shop.scad
+        openscad -o $O/$letter-$which.stl -Dletter='"'$letter'"' -Dwhich=$which -Dis_big=$is_big ./paw-shop.scad
     done
 done
